2b964ca9500ff3357625add200a6ee94a2d48203
Cyber Knight <cyberknight755@gmail.com>
2026-04-08 20:58:20 +0800

    sunny: Sync with DT
    - Switch back to OSS camera provider.
    
    Change-Id: I5e21a3b06ea574562cf485ffbca29ad81946a904
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- sunny-vendor.mk
+++ sunny-vendor.mk
@@ -449,7 +449,6 @@ PRODUCT_PACKAGES += \
     libqcreverb \
     libqcvirt \
     libshoebox \
-    android.hardware.camera.provider@2.4-legacy \
     com.qti.sensor.imx318 \
     com.qti.sensor.ofilm_gc02m1 \
     com.qti.sensor.ofilm_imx355 \
